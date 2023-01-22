6a4a1c88d2d817c693b8236963e3c0fe58d8c2bb
Fritz <furitsucode@gmail.com>
2023-01-23 06:00:07 +0700

    Create calculating-with-functions.py

---
+++ 5kyu/calculating-with-functions.py
@@ -0,0 +1,18 @@
+# Codewars: Calculating with Functions
+# Source: https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/python
+
+def zero(n = 0):  return 0 if n == 0 else int(eval(f'0{n}'))
+def one(n = 0):   return 1 if n == 0 else int(eval(f'1{n}'))
+def two(n = 0):   return 2 if n == 0 else int(eval(f'2{n}'))
+def three(n = 0): return 3 if n == 0 else int(eval(f'3{n}'))
+def four(n = 0):  return 4 if n == 0 else int(eval(f'4{n}'))
+def five(n = 0):  return 5 if n == 0 else int(eval(f'5{n}'))
+def six(n = 0):   return 6 if n == 0 else int(eval(f'6{n}'))
+def seven(n = 0): return 7 if n == 0 else int(eval(f'7{n}'))
+def eight(n = 0): return 8 if n == 0 else int(eval(f'8{n}'))
+def nine(n = 0):  return 9 if n == 0 else int(eval(f'9{n}'))
+
+def plus(n):       return f'+{n}'
+def minus(n):      return f'-{n}'
+def times(n):      return f'*{n}'
+def divided_by(n): return f'/{n}'
